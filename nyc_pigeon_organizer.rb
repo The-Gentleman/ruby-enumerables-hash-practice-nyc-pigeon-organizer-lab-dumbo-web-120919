be2c3a3ba289da9f5cffdb9533cb90f7aa6e847d
Zeshan Raja <zraja4402@gmail.com>
2019-12-05 00:21:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+require 'pry'
+# binding.pry 
+male_names = data[:gender][:male]
+female_names = data[:gender][:female]
+names = [male_names, female_names].flatten
+# names = ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky", "Queenie", "Ms. K"] 
+result = {}
+ 
+ names.each do |name|  
+ result[name] = "huh" 
+ end 
+ 
+
+
+
+result   
+end 
+# {:color=>
+#   {:purple=>["Theo", "Peter Jr.", "Lucky"],
+#   :grey=>["Theo", "Peter Jr.", "Ms. K"],   :white=>["Queenie", "Andrew", "Ms. K", "Alex"],   :brown=>["Queenie", "Alex"]}, :gender=>
+#   {:male=>["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+#   :female=>["Queenie", "Ms. K"]},
+# :lives=>
+#   {"Subway"=>["Theo", "Queenie"],
+#   "Central Park"=>["Alex", "Ms. K", "Lucky"],
+#   "Library"=>["Peter Jr."],
+#   "City Hall"=>["Andrew"]}}
